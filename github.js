28513177494c14f4589fb0270d3ff2aabdf257c3
androsh <apanasiuk00@gmail.com>
2018-04-28 18:27:57 +0100

    initial commit

---
+++ github.js
@@ -0,0 +1,32 @@
+class Github {
+  constructor() {
+    this.client_id = "9cf2546ee0ffbb2d5154";
+    this.client_secret = "eef5c41ea38509e6b833f956b22eff02e655ec01";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
